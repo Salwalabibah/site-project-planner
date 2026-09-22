f331fc601151527062033aa7e3bc6840646ada07
Salwalabibah <salwalabibah04@gmail.com>
2026-09-22 21:37:45 +0700

    Implement user authentication features, including email verification, role management, and login/logout functionality

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,22 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::view('/', 'welcome');
+
+Route::view('dashboard', 'dashboard')
+    ->middleware(['auth', 'verified'])
+    ->name('dashboard');
+
+Route::view('profile', 'profile')
+    ->middleware(['auth'])
+    ->name('profile');
+
+Route::middleware(['role:admin'])->group(function () {
+
 });
+
+Route::middleware(['role:member'])->group(function () {
+
+});
+
+require __DIR__.'/auth.php';
